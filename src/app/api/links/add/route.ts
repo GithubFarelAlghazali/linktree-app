579cd0e069bbb6206df7da7ae632d5d78560c8ec
GithubFarelAlghazali <farelgithub@gmail.com>
2025-12-06 22:04:53 +0700

    API
    Add method validator

--- src/app/api/links/add/route.ts
+++ src/app/api/links/add/route.ts
@@ -2,6 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import addData from "@/lib/firebase/services/addData";
 
 export async function POST(request: NextRequest) {
+	if (request.method !== "POST") {
+		return NextResponse.json({ status: false, message: "Method not allowed", statusCode: 405 });
+	}
+
 	const req = await request.json();
 	const res = await addData(req, "links");
 	return NextResponse.json({ status: res.status, message: res.message, statusCode: res.statusCode });
